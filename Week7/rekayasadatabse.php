14bcf61eae6c79a8e92419cc37e468204ff3154e
ItsTungs <105867103+ItsTungs@users.noreply.github.com>
2023-04-17 11:39:48 +0700

    Add files via upload

---
+++ Week7/rekayasadatabse.php
@@ -0,0 +1,23 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "myDb";
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+$sql = "INSERT INTO liga (kode, negara, champion)
+VALUES ('Jer', 'Jerman', '4' )";
+
+if (mysqli_query($conn, $sql)) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+mysqli_close($conn);
+?>
